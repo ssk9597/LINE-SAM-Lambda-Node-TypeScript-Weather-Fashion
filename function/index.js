d3ffd5b501880f23f2c83a9fc6ac94fa12c2030b
sasaki <skyo.97.admj@gmail.com>
2021-07-19 16:25:56 +0900

    Complete sam build, sam deploy

--- function/index.js
+++ function/index.js
@@ -1,15 +1,15 @@
 // import
-import aws from 'aws-sdk';
+const aws = require('aws-sdk');
 
 // SSM
 const ssm = new aws.SSM();
 
-exports.handler = async (event: any, context: any) => {
+exports.handler = async (event, context) => {
   const params = {
     Name: 'LINE_WEATHER_FASHION_CHANNEL_ACCESS_TOKEN',
     WithDecryption: false,
   };
 
   const channelAccessToken = await ssm.getParameter(params).promise();
-  console.log('channelAccessToken: ' + channelAccessToken);
+  console.log('channelAccessToken: ' + JSON.stringify(channelAccessToken.Parameter.Value));
 };
