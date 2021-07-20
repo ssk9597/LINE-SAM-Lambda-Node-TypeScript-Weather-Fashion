0f50d877cfd80fbf66e2ca25d255a67ec71591a3
sasaki <skyo.97.admj@gmail.com>
2021-07-20 13:36:08 +0900

    "What are you wearing today?" I get messages like

--- function/index.js
+++ function/index.js
@@ -1,15 +1,68 @@
+'use strict';
+
 // import
+const line = require('@line/bot-sdk');
 const aws = require('aws-sdk');
 
 // SSM
 const ssm = new aws.SSM();
+const LINE_WEATHER_FASHION_CHANNEL_ACCESS_TOKEN = {
+  Name: 'LINE_WEATHER_FASHION_CHANNEL_ACCESS_TOKEN',
+  WithDecryption: false,
+};
+const LINE_WEATHER_FASHION_CHANNEL_SECRET = {
+  Name: 'LINE_WEATHER_FASHION_CHANNEL_SECRET',
+  WithDecryption: false,
+};
+const LINE_WEATHER_FASHION_WEATHER_API = {
+  Name: 'LINE_WEATHER_FASHION_WEATHER_API',
+  WithDecryption: false,
+};
 
 exports.handler = async (event, context) => {
-  const params = {
-    Name: 'LINE_WEATHER_FASHION_CHANNEL_ACCESS_TOKEN',
-    WithDecryption: false,
-  };
+  // SSM (.env)
+  const CHANNEL_ACCESS_TOKEN = await ssm
+    .getParameter(LINE_WEATHER_FASHION_CHANNEL_ACCESS_TOKEN)
+    .promise();
+  const CHANNEL_SECRET = await ssm.getParameter(LINE_WEATHER_FASHION_CHANNEL_SECRET).promise();
+  const WEATHER_API = await ssm.getParameter(LINE_WEATHER_FASHION_WEATHER_API).promise();
 
-  const channelAccessToken = await ssm.getParameter(params).promise();
-  console.log('channelAccessToken: ' + JSON.stringify(channelAccessToken.Parameter.Value));
+  const channelAccessToken = CHANNEL_ACCESS_TOKEN.Parameter.Value;
+  const channelSecret = CHANNEL_SECRET.Parameter.Value;
+  const weatherApi = WEATHER_API.Parameter.Value;
+
+  // client
+  const clientConfig = {
+    channelAccessToken: channelAccessToken,
+    channelSecret: channelSecret,
+  };
+  const client = new line.Client(clientConfig);
+
+  // post
+  const body = JSON.parse(event.body);
+  const response = body.events[0];
+
+  // action
+  try {
+    await actionButtonOrErrorMessage(response, client);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const actionButtonOrErrorMessage = async (event, client) => {
+  try {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      return;
+    }
+
+    const { replyToken } = event;
+    const { text } = event.message;
+
+    if (text === '今日の洋服は？') {
+    } else {
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
